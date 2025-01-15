e0db68bd000b6a3e4c68cb0cf57630dc55837261
Mh-Monzil <monzil246@gmail.com>
2025-01-15 16:14:34 +0600

    add: profile route

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 
 
 module.exports.authUser = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
     if (!token) {
         return res.status(401).json({message: 'Unauthorized'});
     }
@@ -12,6 +12,9 @@ module.exports.authUser = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await userModel.findById(decoded._id)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
 
         req.user = user;
 
