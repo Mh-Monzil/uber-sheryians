9b43531796715a073923a17ca077e6c29c8f2d1a
Mh-Monzil <monzil246@gmail.com>
2025-01-19 01:32:33 +0600

    add: captain token

---
+++ backend/controllers/captain.controller.js
@@ -0,0 +1,29 @@
+const captainModel = require('../models/captain.model')
+const captainService = require('../services/captain.service')
+const { validationResult } = require('express-validator')
+
+module.exports.registerCaptain = async (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const { fullName, email, password, vehicle } = req.body;
+
+    const hashedPassword = await captainModel.hashedPassword(password);
+
+    const captain = await captainService.createCaptain({
+        firstName: fullName.firstName,
+        lastName: fullName.lastName,
+        email,
+        password: hashedPassword,
+        color: vehicle.color,
+        plate: vehicle.plate,
+        capacity: vehicle.capacity,
+        vehicleType: vehicle.vehicleType,
+    })
+
+    const token = captain.generateAuthToken()
+
+    return res.status(201).json({ captain, token })
+
+}
