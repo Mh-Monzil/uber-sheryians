a2199ed1e9f6e79f500761e7e14cf6f0c4f92d28
Mh-Monzil <monzil246@gmail.com>
2025-01-22 16:22:48 +0600

    add: captains login, profile and logout

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,17 +1,19 @@
 const userModel = require('../models/user.model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const blacklistTokenModel = require('../models/blacklistToken.model')
+const captainModel = require('../models/captain.model')
 
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
     if (!token) {
-        return res.status(401).json({message: 'Unauthorized'});
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    const isBlacklisted = await userModel.findOne({ token });
+    const isBlacklisted = await blacklistTokenModel.findOne({ token });
     if (isBlacklisted) {
-        return res.status(401).json({message: 'Unauthorized'});
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 
     try {
@@ -26,7 +28,35 @@ module.exports.authUser = async (req, res, next) => {
         return next();
 
     } catch (error) {
-        return res.status(401).json({message: 'Unauthorized'});
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const isBlacklisted = await blacklistTokenModel.findOne({ token });
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const captain = await captainModel.findById(decoded._id)
+        if (!captain) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        req.captain = captain;
+
+        return next();
+
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 }
